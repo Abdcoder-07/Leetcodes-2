95287a7e4c7a3d823696b87c0077a5a2c7d9aafc
Abdcoder-07 <k250868@nu.edu.pk>
2025-11-15 22:27:29 +0500

    Update Q1.c

--- Q1.c
+++ Q1.c
@@ -1,24 +1,26 @@
 //MOVES ALL ZEROS TO END
 #include <stdio.h>
-int main() {
-    int n, i,index=0;
-    printf("Enter number of elements in array\n");
-    scanf("%d", &n);
-
-    int arr[n];
-
-    for (i = 0; i < n; i++) {
-        printf("Enter element no %d\n", i + 1);
-        scanf("%d", &arr[i]);
-    }
-    for (i = 0; i < n; i++) {
+void movezeros(int arr[],int size){
+    int i,index=0;
+    for (i = 0; i < size; i++) {
         if (arr[i] != 0) {
             arr[index++] = arr[i];
         }
     }
-    while (index < n) {
+    while (index < size) {
         arr[index++] = 0;
     }
+}
+int main() {
+    int n,i;
+    printf("Enter number of elements in array\n");
+    scanf("%d", &n);
+    int arr[n];
+    for (i = 0; i < n; i++) {
+        printf("Enter element no %d\n", i + 1);
+        scanf("%d", &arr[i]);
+    }
+    movezeros(arr,n);
     for (i = 0; i < n; i++) {
         printf("%d,", arr[i]);
     }
